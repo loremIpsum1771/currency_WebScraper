2f17998b27e7af60532ae2f72b1981facbc81ff8
Ceyron <ceh451@nyu.edu>
2016-08-07 16:15:42 -0400

    json file added

--- webScraper.py
+++ webScraper.py
@@ -14,6 +14,7 @@ rates = []
 exchangeInfo = {}
 countries = {}
 
+#Get Country names and links from the initial page
 table = root.xpath('.//*[@class="statistics"]')[0]  
 for row in table.xpath(".//tr")[1:]:
 	currentLink = row.xpath(".//th/a/@href")[0]
@@ -22,7 +23,7 @@ for row in table.xpath(".//tr")[1:]:
 		if currency.find('\r') == -1:
 			currencyName = currency
 	
-	#print "currentLink: " +  currentLink
+	
 	for name in row.xpath(".//th/a/text()"):
 		if name.find('\r') == -1:
 			currentName = name
@@ -30,43 +31,29 @@ for row in table.xpath(".//tr")[1:]:
 	links.append(currentLink)
 	countryNames.append(currentName)
 	currencies.append(currencyName)
-	#print " current Name: " + currentName
-	#print currentName
-	#print [cell for cell in row.xpath(".//th/a/@href")]
-#print "links: " 
-#print links
+
+#Get remaining dates and exchange rates for each of the countries
 i = 0
 for i in range(0, len(links)):
 	response2 = requests.get(url + links[i])
-	print "currentLink (second loop) : " + (url + links[i])
+	print "currentLink: " + (url + links[i])
 	root2 = fromstring(response2.content) 
 	table2 = root2.xpath('.//*[@class="statistics"]')[0]  
 	for eachRow in table2.xpath(".//tr")[1:]:
-    
 	    currentDate = eachRow.xpath(".//th/text()")[0]
 	    currentRate = (eachRow.xpath(".//td/text()")[0].strip())
-	    #currentRate.replace(" ", "")
-	    #print " currentDate: " + currentDate
-	    #print "currentRate: " + currentRate
 	    dates.append(currentDate)
 	    rates.append(currentRate)
 	tableData = []
 	tableData.append(dates)
 	tableData.append(rates)
 	exchangeInfo[countryNames[i]] = tableData
-	print "type of: " + str(type(rates[0]))
+	
 	dates = []
 	rates = []
 
 
-# print "countryNames: " 
-# print countryNames
 
-# print "rates: "
-# print rates
-
-# print "dates: "
-# print dates
 print "length of names: " + str(len(countryNames))
 
 i = 0
@@ -83,8 +70,7 @@ print "countryNames: "
 print countryNames
 print "dates: "
 print dates
-# json=json.dumps(exchangeInfo)
-# print json	
+
 
 
 with open('data.json', 'w') as outfile:
